503cede0a34ef9616c4d1a5b96e4b4831b6634a8
Tjs12898 <tristanjohnsagucio@gmail.com>
2024-02-08 21:15:08 -0700

    Capstone Project

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/send-email', (req, res) => {
+  const { to, subject, text } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'tristanjohnsagucio@gmail.com',
+      pass: 'Tj$891988',
+    },
+  });
+
+  const mailOptions = {
+    from: 'tristanjohnsagucio@gmail.com',
+    to,
+    subject,
+    text,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.status(200).send('Email sent: ' + info.response);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
